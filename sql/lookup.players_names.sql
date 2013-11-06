89621e6b918083c945952ca6847e196c38a808c5
Wells Oliver <wellsoliver@gmail.com>
2013-11-05 17:16:07 -0800

    - Added a .gitignore - Added a new lookup table to store variations on a player's name since NHL.com isn't consistent w/ their various reports - Updated my audit script - Added some views for postgres

---
+++ sql/lookup.players_names.sql
@@ -0,0 +1,24 @@
+/*
+*
+* These are alternative player names used by NHL.com (different than the ones
+* stored in the players table). This is populated on a random basis as I find them.
+*
+*/
+
+SET search_path to nhl;
+
+DROP TABLE IF EXISTS players_names;
+
+CREATE TABLE players_names (
+    player_id integer references nhl.players(player_id),
+    player_name text,
+    primary key (player_id, player_name)
+);
+
+INSERT INTO players_names VALUES
+    (8464975, 'DANNY BRIERE'),
+    (8466149, 'DANNY CLEARY'),
+    (8469500, 'MICHAEL CAMMALLERI'),
+    (8471269, 'NICKLAS GROSSMAN'), -- Just a mispelling w/o the extra n!
+    (8467899, 'MARTIN HAVLAT'),
+    (8470640, 'MATT CARLE');
